3b48cf500a9bd24c517d4870f2068eef558dec7c
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-10-20 12:03:05 -0700

    Main header file

--- main.h
+++ main.h
@@ -6,8 +6,18 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define BUFF_SIZE 1024
+typedef struct specifier
+{
+	char *count;
+	int (*f)(va_list);
+} func;
 
 int _printf(const char *format, ...);
+int (*print_function(char a))(va_list);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_d(va_list args);
+int print_i(va_list args);
+int print_percent(va_list args);
 
 #endif /* MAIN_h */
