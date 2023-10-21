562a0c08a57a1439bf3b958c8b3d628361f27304
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-10-21 05:57:09 -0700

    specifier function

---
+++ print_function.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_function - function for the specifier to print
+ * @a: variable to the function
+ * Return: function
+ */
+int (*print_function(char a))(va_list)
+{
+	int x = 0;
+	func arr[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"%", print_percent},
+		{"d", print_d},
+		{"i", print_i},
+		{0, 0}
+	};
+	while (arr[x].count)
+	{
+		if (a == arr[x].count[0])
+			return (arr[x].f);
+		x++;
+	}
+	return (NULL);
+}
