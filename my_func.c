77b33d9cd838b004a1ea03aa029adc613ecfc92a
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-10-22 16:13:50 -0700

    print function

--- my_func.c
+++ my_func.c
@@ -13,6 +13,7 @@ int print_c(va_list args)
 	a = va_arg(args, int);
 	return (_putchar(a));
 }
+
 /**
  * print_s - prints a string
  * @args: string  argument
@@ -20,7 +21,8 @@ int print_c(va_list args)
  */
 int print_s(va_list args)
 {
-	int a, count = 0;
+	int a;
+	int s = 0;
 	char *str;
 
 	a = 0;
@@ -31,16 +33,17 @@ int print_s(va_list args)
 	{
 		_putchar(str[a]);
 		a++;
-		count++;
+		s++;
 	}
-	return (count);
+	return (s);
 }
+
 /**
- * print_percent - pass the percent sing
+ * print_percent - program that pass the percent sing
  * @args: string  argument
  * Return: return the percent sing
- *
  */
+
 int print_percent(va_list args)
 {
 	char *str;
@@ -54,17 +57,17 @@ int print_percent(va_list args)
 }
 
 /**
- * print_d - prints a decimal
+ * print_d - program that prints a decimal
  * @args: decimal argument
- * Return: counter
+ * Return: argument
  */
+
 int print_d(va_list args)
 {
-
-	unsigned int a, step, step_num, count;
+	unsigned int a, step, step_num, s;
 	int b;
 
-	count = 0;
+	s = 0;
 	b = va_arg(args, int);
 		if (b > 0)
 		{
@@ -73,23 +76,26 @@ int print_d(va_list args)
 		else
 		{
 			a = (b * -1);
-			count += _putchar('-');
+			s = s + _putchar('-');
 		}
 
 	step = a;
 	step_num = 1;
+
 	while (step > 9)
-	{
-		step /= 10;
-		step_num *= 10;
+        {
+		step = step / 10;
+		step_num = step_num * 10;
 	}
+
 	while (step_num >= 1)
 	{
-		count += _putchar(((a / step_num) % 10) + '0');
-		step_num /= 10;
+		s = s +  _putchar(((a / step_num) % 10) + '0');
+		step_num = step_num / 10;
 	}
-	return (count);
+	return (s);
 }
+
 /**
  * print_i - prints integer
  * @args: integer argument
