cb0cc053abc81621c3e57dfa83845aa955f33d1a
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-10-23 04:10:22 -0700

    Header file

--- main.h
+++ main.h
@@ -26,7 +26,7 @@ int print_c(va_list args);
 int print_s(va_list args);
 int print_d(va_list args);
 int print_i(va_list args);
-int print_percent(va_list args);
+int print_modulo(va_list args);
 int _putchar(char c);
 
 #endif /* MAIN_h */
